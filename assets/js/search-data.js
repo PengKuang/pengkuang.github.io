eabdc243bfd3a57b0e59dcb8cfed1eac285108ee
PengKuang <31425596+PengKuang@users.noreply.github.com>
2025-04-17 20:00:48 +0000

    Deploying to gh-pages from @ PengKuang/pengkuang.github.io@7cf54c576ef7cb3366e6232402965998f832394b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%61%70.%6B%75%61%6E%67@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
